6528c45ed53a5e1ecc70337fb5ab6447eac54513
andy23512 <andy23512@gmail.com>
2020-04-16 23:07:52 +0800

    Solve path bug

--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 import json
+import os
 import requests
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def json_requests_factory(site: str):
     if site == 'gitlab':
         api_url = 'https://gitlab.com/api/v4'
-        with open('./gitlab_token') as f:
+        with open(os.path.join(dir_path, 'gitlab_token')) as f:
             headers = {'Private-Token': f.readline().rstrip('\n\r')}
     elif site == 'clickup':
         api_url = 'https://api.clickup.com/api/v2'
-        with open('./clickup_token') as f:
+        with open(os.path.join(dir_path, 'clickup_token')) as f:
             headers = {'Authorization': f.readline().rstrip("\n\r")}
     else:
         raise ValueError('This site is not supported')
