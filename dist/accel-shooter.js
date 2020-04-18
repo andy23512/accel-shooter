f2b2f828e219508c23a50d0d89ee463012227db2
andy23512 <andy23512@gmail.com>
2020-04-18 13:20:28 +0800

    Add setConfigFile feature

--- dist/accel-shooter.js
+++ dist/accel-shooter.js
@@ -8,12 +8,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-() => __awaiter(void 0, void 0, void 0, function* () {
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const path_1 = require("path");
+(() => __awaiter(void 0, void 0, void 0, function* () {
     const action = process.argv[2];
     switch (action) {
         case 'config':
+            const configFile = process.argv[3];
+            setConfigFile(configFile);
+            break;
         case 'start':
         default:
             throw Error(`Action {action} is not supported`);
     }
-});
+}))();
+function setConfigFile(configFile) {
+    const src = path_1.resolve(configFile);
+    const dest = path_1.resolve(__dirname, '../.config.json');
+    fs.copyFileSync(src, dest);
+}
--- src/accel-shooter.ts
+++ src/accel-shooter.ts
@@ -1,9 +1,20 @@
-async () => {
+import * as fs from 'fs';
+import { resolve as pathResolve } from 'path';
+(async () => {
   const action = process.argv[2];
   switch (action) {
     case 'config':
+      const configFile = process.argv[3];
+      setConfigFile(configFile);
+      break;
     case 'start':
     default:
       throw Error(`Action {action} is not supported`);
   }
-};
+})();
+
+function setConfigFile(configFile: string) {
+  const src = pathResolve(configFile);
+  const dest = pathResolve(__dirname, '../.config.json');
+  fs.copyFileSync(src, dest);
+}
