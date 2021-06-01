321cd0e0089aec2a09d761cd6c46fdd224890340
andy23512 <andy23512@gmail.com>
2021-06-01 09:19:28 +0800

    Make check item class

--- src/checker.ts
+++ src/checker.ts
@@ -1,116 +1,106 @@
 import os from "os";
+import { concat, defer, of } from "rxjs";
 import { GitLab } from "./gitlab";
 import { Change, MergeRequest } from "./models/gitlab/merge-request.models";
 import { GitLabProject } from "./models/models";
 import { promiseSpawn } from "./utils";
 
-type CheckItem = {
-  group: string;
-  name: string;
-  run: (context: {
-    mergeRequest: MergeRequest;
-    gitLab: GitLab;
-    frontendChanges: Change[];
-    backendChanges: Change[];
-  }) => Promise<{ stdout?: string; stderr?: string; code: number }>;
-};
+interface CheckContext {
+  mergeRequest: MergeRequest;
+  gitLab: GitLab;
+  frontendChanges: Change[];
+  backendChanges: Change[];
+}
+
+class CheckItem {
+  constructor(
+    public group: string,
+    public name: string,
+    public run: (
+      context: CheckContext
+    ) => Promise<{ stdout?: string; stderr?: string; code: number }>
+  ) {}
+
+  public getObs(context: CheckContext) {
+    return concat(
+      of({ code: -1 }),
+      defer(() => this.run(context))
+    );
+  }
+}
 
 const items: CheckItem[] = [
-  {
-    group: "Global",
-    name: "Check Non-Pushed Changes",
-    async run() {
-      const result = await promiseSpawn("git", ["status"], "pipe");
-      result.code =
-        result.stdout.includes("Your branch is up to date with") &&
-        result.stdout.includes("nothing to commit, working tree clean")
-          ? 0
-          : 1;
-      return result;
-    },
-  },
-  {
-    group: "Global",
-    name: "Check Conflict",
-    async run({ mergeRequest, gitLab }) {
+  new CheckItem("Global", "Check Non-Pushed Changes", async () => {
+    const result = await promiseSpawn("git", ["status"], "pipe");
+    result.code =
+      result.stdout.includes("Your branch is up to date with") &&
+      result.stdout.includes("nothing to commit, working tree clean")
+        ? 0
+        : 1;
+    return result;
+  }),
+  new CheckItem(
+    "Global",
+    "Check Conflict",
+    async ({ mergeRequest, gitLab }) => {
       const fullMergeRequest = await gitLab.getMergeRequest(mergeRequest.iid);
       const isConflict = fullMergeRequest.has_conflicts;
       return { code: isConflict ? 1 : 0 };
-    },
-  },
-  {
-    group: "Frontend",
-    name: "Check Lint",
-    async run() {
-      return promiseSpawn(
-        "docker-compose",
-        ["exec", "frontend", "yarn", "lint"],
-        "pipe"
-      );
-    },
-  },
-  {
-    group: "Frontend",
-    name: "Check Test",
-    async run() {
-      return promiseSpawn(
-        "docker-compose",
-        ["exec", "frontend", "yarn", "jest", "--coverage=false"],
-        "pipe"
-      );
-    },
-  },
-  {
-    group: "Frontend",
-    name: "Check Prod",
-    async run() {
-      return promiseSpawn(
-        "docker-compose",
-        ["exec", "frontend", "yarn", "prod"],
-        "pipe"
-      );
-    },
-  },
-  {
-    group: "Frontend",
-    name: "Check console.log",
-    async run({ frontendChanges }) {
+    }
+  ),
+  new CheckItem("Frontend", "Check Lint", async () => {
+    return promiseSpawn(
+      "docker-compose",
+      ["exec", "frontend", "yarn", "lint"],
+      "pipe"
+    );
+  }),
+  new CheckItem("Frontend", "Check Test", async () => {
+    return promiseSpawn(
+      "docker-compose",
+      ["exec", "frontend", "yarn", "jest", "--coverage=false"],
+      "pipe"
+    );
+  }),
+  new CheckItem("Frontend", "Check Prod", async () => {
+    return promiseSpawn(
+      "docker-compose",
+      ["exec", "frontend", "yarn", "prod"],
+      "pipe"
+    );
+  }),
+  new CheckItem(
+    "Frontend",
+    "Check console.log",
+    async ({ frontendChanges }) => {
       return {
         code: frontendChanges.some((c) => c.diff.includes("console.log"))
           ? 1
           : 0,
       };
-    },
-  },
-  {
-    group: "Frontend",
-    name: "Check long import",
-    async run({ frontendChanges }) {
+    }
+  ),
+  new CheckItem(
+    "Frontend",
+    "Check long import",
+    async ({ frontendChanges }) => {
       return {
         code: frontendChanges.some((c) => c.diff.includes("../../")) ? 1 : 0,
       };
-    },
-  },
-  {
-    group: "Backend",
-    name: "Check Test",
-    async run() {
-      return promiseSpawn(
-        "docker-compose",
-        ["exec", "-T", "backend", "pytest", "."],
-        "pipe"
-      );
-    },
-  },
-  {
-    group: "Backend",
-    name: "Check print",
-    async run({ backendChanges }) {
-      return {
-        code: backendChanges.some((c) => c.diff.includes("print(")) ? 1 : 0,
-      };
-    },
-  },
+    }
+  ),
+  new CheckItem("Backend", "Check Test", async () => {
+    return promiseSpawn(
+      "docker-compose",
+      ["exec", "-T", "backend", "pytest", "."],
+      "pipe"
+    );
+  }),
+  new CheckItem("Backend", "Check print", async ({ backendChanges }) => {
+    return {
+      code: backendChanges.some((c) => c.diff.includes("print(")) ? 1 : 0,
+    };
+  }),
 ];
 
 export class Checker {
@@ -150,7 +140,13 @@ export class Checker {
       runningItems = items.filter((item) => item.group !== "Frontend");
     }
     if (backendChanges.length === 0) {
-      runningItems = items.filter((item) => item.group !== "Frontend");
+      runningItems = items.filter((item) => item.group !== "Backend");
     }
+    const context = {
+      mergeRequest,
+      gitLab: this.gitLab,
+      frontendChanges,
+      backendChanges,
+    };
   }
 }
