bd897e3c574fa4a8c638f137c470ba4e11ab75c1
andy23512 <andy23512@gmail.com>
2021-08-30 14:38:45 +0800

    fix(extension): keep case in url param parsing

--- apps/extension/main.ts
+++ apps/extension/main.ts
@@ -1,6 +1,6 @@
 function getAllUrlParams<T>(url: string): T {
   // get query string from url (optional) or window
-  let queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  let queryString = url ? url.split("?")[1] : window.location.search.slice(1);
 
   // we'll store the parameters here
   const obj = {};
@@ -8,27 +8,23 @@ function getAllUrlParams<T>(url: string): T {
   // if query string exists
   if (queryString) {
     // stuff after # is not part of query string, so get rid of it
-    queryString = queryString.split('#')[0];
+    queryString = queryString.split("#")[0];
 
     // split our query string into its component parts
-    const arr = queryString.split('&');
+    const arr = queryString.split("&");
 
     for (const part of arr) {
       // separate the keys and the values
-      const a = part.split('=');
+      const a = part.split("=");
 
       // set parameter name and value (use 'true' if empty)
-      let paramName = a[0];
-      let paramValue = typeof a[1] === 'undefined' ? true : a[1];
-
-      // (optional) keep case consistent
-      paramName = paramName.toLowerCase();
-      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+      const paramName = a[0];
+      const paramValue = typeof a[1] === "undefined" ? true : a[1];
 
       // if the paramName ends with square brackets, e.g. colors[] or colors[2]
       if (paramName.match(/\[(\d+)?\]$/)) {
         // create key if it doesn't exist
-        const key = paramName.replace(/\[(\d+)?\]/, '');
+        const key = paramName.replace(/\[(\d+)?\]/, "");
         if (!obj[key]) obj[key] = [];
 
         // if it's an indexed array e.g. colors[2]
@@ -45,7 +41,7 @@ function getAllUrlParams<T>(url: string): T {
         if (!obj[paramName]) {
           // if it doesn't exist, create property
           obj[paramName] = paramValue;
-        } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+        } else if (obj[paramName] && typeof obj[paramName] === "string") {
           // if property does exist and it's a string, convert it to an array
           obj[paramName] = [obj[paramName]];
           obj[paramName].push(paramValue);
@@ -63,7 +59,7 @@ function getAllUrlParams<T>(url: string): T {
 chrome.tabs.onCreated.addListener(async (tab) => {
   const isAccelShooterLink =
     tab.pendingUrl &&
-    tab.pendingUrl.startsWith('http://localhost:8315/accel-shooter/');
+    tab.pendingUrl.startsWith("http://localhost:8315/accel-shooter/");
   console.log(isAccelShooterLink);
   if (isAccelShooterLink) {
     const query = getAllUrlParams<{ urls: string; group: string }>(
@@ -79,7 +75,7 @@ chrome.tabs.onCreated.addListener(async (tab) => {
     }
     const groupId = await chrome.tabs.group({ tabIds: tabIds });
     chrome.tabGroups.move(groupId, { index: 0 });
-    chrome.tabGroups.update(groupId, { color: 'cyan', title: group });
+    chrome.tabGroups.update(groupId, { color: "cyan", title: group });
     chrome.tabs.remove(tab.id);
   }
 });
--- dist/apps/extension/main.js
+++ dist/apps/extension/main.js
@@ -9,29 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 function getAllUrlParams(url) {
     // get query string from url (optional) or window
-    let queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    let queryString = url ? url.split("?")[1] : window.location.search.slice(1);
     // we'll store the parameters here
     const obj = {};
     // if query string exists
     if (queryString) {
         // stuff after # is not part of query string, so get rid of it
-        queryString = queryString.split('#')[0];
+        queryString = queryString.split("#")[0];
         // split our query string into its component parts
-        const arr = queryString.split('&');
+        const arr = queryString.split("&");
         for (const part of arr) {
             // separate the keys and the values
-            const a = part.split('=');
+            const a = part.split("=");
             // set parameter name and value (use 'true' if empty)
-            let paramName = a[0];
-            let paramValue = typeof a[1] === 'undefined' ? true : a[1];
-            // (optional) keep case consistent
-            paramName = paramName.toLowerCase();
-            if (typeof paramValue === 'string')
-                paramValue = paramValue.toLowerCase();
+            const paramName = a[0];
+            const paramValue = typeof a[1] === "undefined" ? true : a[1];
             // if the paramName ends with square brackets, e.g. colors[] or colors[2]
             if (paramName.match(/\[(\d+)?\]$/)) {
                 // create key if it doesn't exist
-                const key = paramName.replace(/\[(\d+)?\]/, '');
+                const key = paramName.replace(/\[(\d+)?\]/, "");
                 if (!obj[key])
                     obj[key] = [];
                 // if it's an indexed array e.g. colors[2]
@@ -51,7 +47,7 @@ function getAllUrlParams(url) {
                     // if it doesn't exist, create property
                     obj[paramName] = paramValue;
                 }
-                else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                else if (obj[paramName] && typeof obj[paramName] === "string") {
                     // if property does exist and it's a string, convert it to an array
                     obj[paramName] = [obj[paramName]];
                     obj[paramName].push(paramValue);
@@ -67,7 +63,7 @@ function getAllUrlParams(url) {
 }
 chrome.tabs.onCreated.addListener((tab) => __awaiter(this, void 0, void 0, function* () {
     const isAccelShooterLink = tab.pendingUrl &&
-        tab.pendingUrl.startsWith('http://localhost:8315/accel-shooter/');
+        tab.pendingUrl.startsWith("http://localhost:8315/accel-shooter/");
     console.log(isAccelShooterLink);
     if (isAccelShooterLink) {
         const query = getAllUrlParams(tab.pendingUrl);
@@ -81,7 +77,7 @@ chrome.tabs.onCreated.addListener((tab) => __awaiter(this, void 0, void 0, funct
         }
         const groupId = yield chrome.tabs.group({ tabIds: tabIds });
         chrome.tabGroups.move(groupId, { index: 0 });
-        chrome.tabGroups.update(groupId, { color: 'cyan', title: group });
+        chrome.tabGroups.update(groupId, { color: "cyan", title: group });
         chrome.tabs.remove(tab.id);
     }
 }));
