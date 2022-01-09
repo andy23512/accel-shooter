ef34af9f9ee59fee7e4049c60ecfe0e74b5cc8a4
andy23512 <andy23512@gmail.com>
2022-01-09 23:49:27 +0800

    chore(frontend): install angular material

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { CodemirrorModule } from "@ctrl/ngx-codemirror";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, CodemirrorModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, CodemirrorModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
