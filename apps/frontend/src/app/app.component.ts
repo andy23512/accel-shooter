752d38efe7609ef5d0118621a708855ca90fef71
andy23512 <andy23512@gmail.com>
2022-01-09 22:18:26 +0800

    feat(frontend): convert clickup task checklist to markdown

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@accel-shooter/api-interfaces';
+import {
+  ChecklistItem,
+  NormalizedChecklist,
+  Task,
+} from "@accel-shooter/node-shared";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+
+export function normalizeClickUpChecklist(
+  checklist: ChecklistItem[]
+): NormalizedChecklist {
+  return checklist
+    .sort((a, b) => a.orderindex - b.orderindex)
+    .map((item, index) => ({
+      name: item.name,
+      checked: item.resolved,
+      order: index,
+      id: item.id,
+    }));
+}
 
 @Component({
-  selector: 'accel-shooter-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "accel-shooter-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+export class AppComponent implements OnInit {
+  public task$ = this.http.get<Task>("/api/task/aj55xx");
   constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.task$.subscribe((task) => {
+      const targetChecklist = task.checklists.find((c) =>
+        c.name.toLowerCase().includes("synced checklist")
+      );
+      if (targetChecklist) {
+        const clickUpNormalizedChecklist = normalizeClickUpChecklist(
+          targetChecklist.items
+        );
+        const checklistMarkdown = clickUpNormalizedChecklist
+          .map((c) =>
+            c.name.replace(
+              /^-*/,
+              (dashes) =>
+                dashes.replace(/-/g, " ") + (c.checked ? "- [x] " : "- [ ] ")
+            )
+          )
+          .join("\n");
+        console.log(checklistMarkdown);
+      }
+    });
+  }
 }
