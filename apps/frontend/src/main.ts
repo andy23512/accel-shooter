ff3c7893356a62ae1c21ff54994beb35e7632c53
andy23512 <andy23512@gmail.com>
2022-10-02 09:17:14 +0800

    feat(frontend): built-in darkreader

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -3,8 +3,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'codemirror/addon/edit/continuelist';
 import 'codemirror/keymap/vim';
 import 'codemirror/mode/gfm/gfm';
+import * as darkreader from 'darkreader';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+darkreader.enable(
+  {},
+  {
+    invert: [],
+    css: `.cm-s-one-dark .CodeMirror-cursor, .cm-fat-cursor .CodeMirror-cursor {
+  border-left-color: #fff !important;
+  background: #fff !important;
+}`,
+    ignoreInlineStyle: [],
+    ignoreImageAnalysis: [],
+    disableStyleSheetsProxy: false,
+  }
+);
 
 if (environment.production) {
   enableProdMode();
